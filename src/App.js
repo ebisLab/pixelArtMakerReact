ab704052ff0bfc57565a7591515ab283db650ba2
ebisLab <ebaiden@techie.com>
2020-11-17 13:04:04 -0500

    width and height can now be set by user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Grid from './components/Grid';
 import ColorPicker from './components/Colorpicker'
 import useStyles from './App.styles'
@@ -10,38 +10,75 @@ const offCell={
   color: "yellow",
 
 }
-const initialCells = Array.from({length:40},()=>offCell)
+
 
 function App() {
+
+  const [val, setVal]=useState({
+    width:'',
+    height:''
+  })
+  const [val2, setVal2]=useState()
+  const [forceRerender, setForceRerender] = useState(false);
+  useEffect(() => {
+setVal2(val2)
+setForceRerender(false);
+  }, [val2, val, forceRerender])
+
+  const initialCells = Array.from({length:100},()=>offCell)
   const [cells, setCells]=useState(initialCells)
   const [currentColor, setCurrentColor] = useState("#56bc58");
-  // const [colorHistory, setColorHistory]=useState([])
-
   const classes= useStyles();
-  // const onSetColor = (color)=>{
-  //   const newcolors = colorHistory.slice(-6).concat(color)
-  //   setColorHistory(newcolors)
-  //   setCurrentColor(color)
 
-  // }
+  const setNewVal=(e)=>{
+    e.preventDefault()
+    setVal2(val)
+    setCells(Array.from({length:val.width*val.height},()=>offCell))
+    setForceRerender(true);
+  }
+
 
   return (
+    <div className="App">
+    <h1>Pixel Art Maker</h1>
+    <form onSubmit={setNewVal}>
+        Grid Width: 
+              <input 
+              value={val.width} 
+              placeholder="width"
+              name="width"
+              type="number"
+              onChange={(e)=>(setVal({...val, [e.target.name]: e.target.value})
+              )}/>
+              Grid Height:
+              <input 
+              value={val.height} 
+              placeholder="height"
+              name="height"
+              type="number"
+              onChange={(e)=>(setVal({...val, [e.target.name]: e.target.value})
+              )}/>
+              <button>submit</button>
+      </form>
     <div className={classes.app}>
-      <ColorPicker currentColor={currentColor} onSetColor={setCurrentColor} 
-      // setCurrentColor={setCurrentColor} 
+
+   
+      <ColorPicker  currentColor={currentColor} onSetColor={setCurrentColor} 
       />
-         <div className={classes.colorSwatchContainer}>
+         <div className={classes.colorSwatchContainer} style={{height:"60px"}}>
 
            {[...new Set(cells.filter((cell)=>cell.on).map((cell)=>cell.color))].map((color,i)=>
            <div key={i} onClick={()=>setCurrentColor(color)} className={classes.colorSwatch} style={{background: color}}></div>
          )}
          </div>
-         
-          <Grid 
+{ !forceRerender && val2 && <Grid 
+          val2={val2}
           currentColor={currentColor}
           cells={cells}
           setCells={setCells}
            />
+           }
+    </div>
     </div>
 
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import useStyles from '../Grid.styles'
+import {createUseStyles } from 'react-jss'
 
 const offCell={
     on:false,
     color: "yellow",
 
 }
-// const initialCells = Array.from({length:40},()=>offCell)
 
-export default function Grid({currentColor, cells, setCells}) {
-    // const [cells, setCells]=useState(initialCells)
+export default function Grid({currentColor, cells, setCells, val2}) {
+    console.log("val-->", Number(val2))
+
+    const useStyles2 = createUseStyles({
+        grid2:{
+            background:"red",
+            display:"grid",
+            gridTemplateRows:`repeat(${Number(val2.height)}, 1fr)`,
+            gridTemplateColumns:`repeat(${Number(val2.width)}, 1fr)`,
+            outline:"1px solid black",
+        },
+    
+      })
 
     const classes= useStyles();
+    const classes2= useStyles2();
+
     const updateCell=(i, defaultState)=>(e)=>{
         e.preventDefault()
         setCells(cells.map((cell,cellindex)=>{
@@ -23,9 +36,12 @@ export default function Grid({currentColor, cells, setCells}) {
         }))
     };
 
+
+    
+
     return (
-        <div className={classes.grid}>
-            {/* {Array.from({length:40})} */}
+        <>
+        <div className={classes2.grid2}>
             {cells.map((cell, i)=><div 
             style={{background:cell.on? cell.color: "#ffffff"}}
             onClick={updateCell(i)} 
@@ -33,5 +49,6 @@ export default function Grid({currentColor, cells, setCells}) {
             key={i} className={classes.cell}></div>)}
             
         </div>
+        </>
     )
 }
